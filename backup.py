3821acdfec037b0e65ac7613a758b112ec7e346b
Andrei <andrei1015@gmail.com>
2021-03-07 07:04:30 +0100

    adding works

--- backup.py
+++ backup.py
@@ -5,36 +5,39 @@ import tkinter as tk
 import configparser
 
 config = configparser.ConfigParser()
-list = config.read('backup.cfg')
-
-
+#list = config.read('backup.cfg')
 window = tk.Tk()
 window.title(".dotfiles backup")
 window.geometry("700x700")
 
-def addPath():
+
+file = open("locations", "r")
+list = file.readlines()
+listbox_widget = tk.Listbox(window, listvariable=list)
+
+def addPath(text):
     file = open("locations", "a")
-    file.write('test\n')
+    file.write(text)
+    listbox_widget.insert(tk.END, addField.get() + '\n')
     print('test1')
 
-def refresh():
-    greeting.update()
+# def refresh():
+# 	listbox_widget.delete(0, tk.END)
+# 	for path in list:
+# 	    listbox_widget.insert(tk.END, path)
 
 
 addField = tk.Entry(window)
 addField.pack()
 
-addbutton = tk.Button(window, text ="add", command = lambda:[addPath(), refresh()])
+addbutton = tk.Button(window, text ="add", command = lambda:[addPath(addField.get() + '\n')])
 addbutton.pack()
 
-frame = tk.Frame(list, width=100, height=50).pack()
-
-file = open("locations", "r")
-list = file.readlines()
-
 for path in list:
-    greeting = tk.Label(frame, text=path)
-    greeting.pack()
+    listbox_widget.insert(tk.END, path)
+    listbox_widget.pack()
+    # greeting = tk.Label(frame, text=path)
+    # greeting.pack()
 
 
 # for setting in list:
