91685f005dc9a0b8784b0f254222e1b4c8054a13
Andrei <andrei1015@gmail.com>
2021-03-07 15:30:17 +0100

    delete works

--- backup.py
+++ backup.py
@@ -9,18 +9,38 @@ config = configparser.ConfigParser()
 #list = config.read('backup.cfg')
 window = tk.Tk()
 window.title(".dotfiles backup")
+window.resizable(False, False)
 window.geometry("700x700")
-
 
 file = open("locations", "r")
 list = file.readlines()
-listbox_widget = tk.Listbox(window, listvariable=list)
+listbox_widget = tk.Listbox(window, listvariable=list, selectbackground='#ff2400', selectmode=tk.MULTIPLE)
 
 def addPath(text):
     file = open("locations", "a")
     file.writelines(text)
     listbox_widget.insert('end', text.rstrip())
-    print(text)
+    addField.delete(0, 'end')
+    print(listbox_widget.curselection())
+
+def deletePath(text):
+    file = open("locations", "a")
+    file.writelines(text)
+    listbox_widget.insert('end', text.rstrip())
+    addField.delete(0, 'end')
+    print(listbox_widget.curselection())
+
+def showList():
+    for path in list:
+        listbox_widget.insert('end', path.rstrip())
+        listbox_widget.grid(row = 1, column = 5, columnspan = 4)
+
+def deleteSelected(toDelete):
+    for line in toDelete[::-1]:
+        listbox_widget.delete(line)
+        os.system("sed -i '" + str(line+1) + "d' locations")
+        print(line)
+        
 
 # def refresh():
 # 	listbox_widget.delete(0, tk.END)
@@ -28,17 +48,13 @@ def addPath(text):
 # 	    listbox_widget.insert(tk.END, path)
 
 
-addField = tk.Entry(window)
-addField.pack()
+addField = tk.Entry(window).grid(row = 0, column = 0, padx=100, pady=100, ipady=30, sticky = 'nsew')
 
-addbutton = tk.Button(window, text ="add", command = lambda:[addPath(addField.get() + '\n')])
-addbutton.pack()
+addButton = tk.Button(window, text ="add", command = lambda:[addPath(addField.get() + '\n')]).grid(row = 1, column = 1)
 
-for path in list:
-    listbox_widget.insert('end', path.rstrip())
-    listbox_widget.pack()
-    # greeting = tk.Label(frame, text=path)
-    # greeting.pack()
+removeButton = tk.Button(window, text ="remvoe", command = lambda:[deleteSelected(listbox_widget.curselection())]).grid(row = 1, column = 1)
+
+showList()
 
 
 # for setting in list:
